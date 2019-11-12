2c14a691709c204f5baf7b7ab4cd513a45f6569e
Ahmed <kahildude@gmail.com>
2019-11-12 17:18:39 +0200

    1

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const request = require('request')
-const stock = require('../model/Stocks')
+const Stock = require('../model/Stocks')
 const requestPromise = require('request-promise')
-const mongoose = require('mongoose')
+
 
 
 router.get('/stock/:Ticker', async function (req, res) {
@@ -20,21 +19,40 @@ router.get('/stock/:Ticker', async function (req, res) {
             'Ocp-Apim-Subscription-Key': '9d6df703d4f14735afe82789694f959e'
         }
     };
+    let priceReq = {
+        url: "https://services.last10k.com/v1/company/" + ticker + "/quote",
+        headers: {
+            'Ocp-Apim-Subscription-Key': '9d6df703d4f14735afe82789694f959e'
+        }
+    };
+    let priceData
     let balanceSheetData
     let incomeData
     try {
         balanceSheetData = await requestPromise(balanceSheetReq)
         incomeData = await requestPromise(incomeReq)
+        priceData = await requestPromise(priceReq)
         balanceSheetData  = JSON.parse(balanceSheetData)
         incomeData  = JSON.parse(incomeData)
+        priceData = JSON.parse(priceData)
     }
     catch (err) {
         console.log(err)
     }
     companyData = {company:balanceSheetData.Company,
-                   balanceSheetData: balanceSheetData.Data,
-                   incomeData:incomeData.Data}
+                   price: priceData.LastTradePrice,
+                   volume: priceData.Volume,
+                   balanceSheet: balanceSheetData.Data,
+                   income:incomeData.Data,
+                   cashFlow:{},
+                   sector:"",
+                   dividend:1,
+                   marketCap:1
+                }
+    const stock2DB = new Stock(companyData)
+    await stock2DB.save()               
     res.send(JSON.stringify(companyData))
 })
+
 
 module.exports = router
