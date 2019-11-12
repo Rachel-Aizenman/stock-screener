cdfe3c827cdd2de5476206f859389dccefbf9a10
Noam Lior <noamlior2@gmail.com>
2019-11-12 19:22:18 +0200

    data-cleaner v2

--- ratioCalculator.js
+++ ratioCalculator.js
@@ -1,30 +1,35 @@
 function ratioCalculator(stock) {
     let ratios;
-    const netIncome = stock.income.NetIncome
     const equity = stock.balanceSheet.StockholdersEquity
     const revenue = stock.income.Revenue
     const costOfGoods = stock.income.CostOfGoods
     const price = stock.price
     const assets = stock.balanceSheet.Assets
     const liabilities = stock.balanceSheet.Liabilities
-    const grossProfit = stock.income.grossProfit
-    const operationalProfit = stock.income.OperationalProfit
-    const netProfit = stock.income.NetProfit
-    const longTermDebt= stock.balanceSheet.LongTermDebtCurrent+ stock.balanceSheet.LongTermDebtNoncurrent 
+    const longTermDebt = stock.balanceSheet.LongTermDebtCurrent + stock.balanceSheet.LongTermDebtNoncurrent
+
+    const grossProfit = stock.income.grossProfit // if exists
+    const operationalProfit = stock.income.OperationalProfit // if exists
+    const netIncome = stock.income.NetIncome 
+    
     
     ratios[ROE] = netIncome / equity
     ratios[ROI] = (revenue - costOfGoods) / costOfGoods
-    
-    ratios[PE] = price / netProfit
+
+    ratios[PE] = price / netIncome
     ratios[PB] = price / (assets - liabilities)
     ratios[PS] = price / revenue
-    
+
     ratios[grossProfitMargin] = grossProfit / revenues
     ratios[operationalProfitMargin] = operationalProfit / revenues
-    ratios[netProfitMargin] = netProfit / revenues
-    
-    ratios[debtToEquity]=(liabilities-equity)/equity
-    ratios[LongTermDebtToEquity]=longTermDebt/equity
+    ratios[netIncomeMargin] = netIncome / revenues
 
+    ratios[debtToEquity] = (liabilities - equity) / equity
+    ratios[LongTermDebtToEquity] = longTermDebt / equity
+
+    const ratioKeys = Object.keys(ratios);
+    for (ratio of ratioKeys)
+        if (ratios[ratio] < 0)
+            ratios[ratio] = 0;
     return ratios;
 }
