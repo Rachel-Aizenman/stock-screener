bbb63f49ba97a029273ebca75116187e7761d373
Rachel Aizenman <racheliamn@gmail.com>
2019-11-13 15:41:49 +0200

    v1

--- dist/main.js
+++ dist/main.js
@@ -1,18 +1,24 @@
-const ratioCalculator= new RatioCalculator();
-const analyzer=new StockAnalyzer();
+const ratioCalculator = new RatioCalculator();
+const analyzer = new StockAnalyzer();
 const stocksManager = new StockManager();
 const renderer = new Renderer
 
-const handleSearch = async function(){
-    let input = $("#search-input").val()
-    $("#search-input").val("") 
+const handleSearch = async function () {
+    let input = $("#stockSearch").val()
+    console.log(input)
+
+    $("#stockSearch").val("")
     await stocksManager.getStockData(input)
     renderer.renderData(stocksManager.stockData)
 }
 
-$("#search").on("click",async function () {
-    await handleSearch()
+let input = $("#stockSearch").val()
+
+$("#searchButton").on('click', async function () {
+ console.log("A")
+    handleSearch()
 })
-const rc= new RatioCalculator();
+
+const rc = new RatioCalculator();
 
 
