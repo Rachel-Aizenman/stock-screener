7cc0ec287437aa70ea0b3ac6c4eccb7b647f4b67
Rachel Aizenman <racheliamn@gmail.com>
2019-11-13 13:18:37 +0200

    solving merge issues with display

--- dist/render.js
+++ dist/render.js
@@ -1,25 +1,44 @@
-const stockData = [{
-  balanceSheet: "balance-sheet here",
-  income: "income here",
-  cashFlow: "cash-flow here"
-}, 
-{
-  balanceSheet: "2 balance-sheet here",
-  income: "2 income here",
-  cashFlow: "2 cash-flow here"
+const DudisObj = [{
+  company: "hivoub",
+   price: 'jbloinli',
+volume: "iyviugkj",
+balanceSheet: "iyviugkj", 
+jsonIncome: "iyviugkj",
+cashflow: "iyviugkj",
+sector: "iyviugkj",
+dividend: "iyviugkj",
+marketCap: "iyviugkj"
 }, {
-  balanceSheet: "3 balance-sheet here",
-  income: "3 income here",
-  cashFlow: "3 cash-flow here"
+  company: "hivoub",
+   price: 'jbloinli',
+volume: "iyviugkj",
+balanceSheet: "iyviugkj", 
+jsonIncome: "iyviugkj",
+cashflow: "iyviugkj",
+sector: "iyviugkj",
+dividend: "iyviugkj",
+marketCap: "iyviugkj"
+}, {
+  company: "hivoub",
+   price: 'jbloinli',
+volume: "iyviugkj",
+balanceSheet: "iyviugkj", 
+jsonIncome: "iyviugkj",
+cashflow: "iyviugkj",
+sector: "iyviugkj",
+dividend: "iyviugkj",
+marketCap: "iyviugkj"
 }]
 
 class Renderer{
     renderData (stockData) {
         $("#search-input").val("") 
         $("#display-stocks").empty()
+        console.log(stockData)
           const source = $("#stocks-template").html()
           const template = Handlebars.compile(source)
           const someHTML = template({stockData}) //check the format in which you recieve it
+          console.log(someHTML)
           $("#display-stocks").append(someHTML)
        }
 }
@@ -27,6 +46,11 @@ class Renderer{
 const renderer = new Renderer()
  
 const pageLoad = function(){
-  renderer.renderData(stockData)}
+  renderer.renderData(DudisObj)}
 
 pageLoad()
+
+
+// Handlebars.registerHelper('json', function(context) {
+//   return JSON.stringify(context);
+// });
