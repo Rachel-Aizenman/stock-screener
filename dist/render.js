49f2c9881bc026442917bf6657e7fd390c1347f8
Rachel Aizenman <racheliamn@gmail.com>
2019-11-13 13:30:41 +0200

    for smooth merging

--- dist/render.js
+++ dist/render.js
@@ -1,56 +1,10 @@
-const DudisObj = [{
-  company: "hivoub",
-   price: 'jbloinli',
-volume: "iyviugkj",
-balanceSheet: "iyviugkj", 
-jsonIncome: "iyviugkj",
-cashflow: "iyviugkj",
-sector: "iyviugkj",
-dividend: "iyviugkj",
-marketCap: "iyviugkj"
-}, {
-  company: "hivoub",
-   price: 'jbloinli',
-volume: "iyviugkj",
-balanceSheet: "iyviugkj", 
-jsonIncome: "iyviugkj",
-cashflow: "iyviugkj",
-sector: "iyviugkj",
-dividend: "iyviugkj",
-marketCap: "iyviugkj"
-}, {
-  company: "hivoub",
-   price: 'jbloinli',
-volume: "iyviugkj",
-balanceSheet: "iyviugkj", 
-jsonIncome: "iyviugkj",
-cashflow: "iyviugkj",
-sector: "iyviugkj",
-dividend: "iyviugkj",
-marketCap: "iyviugkj"
-}]
-
 class Renderer{
     renderData (stockData) {
         $("#search-input").val("") 
         $("#display-stocks").empty()
-        console.log(stockData)
           const source = $("#stocks-template").html()
           const template = Handlebars.compile(source)
-          const someHTML = template({stockData}) //check the format in which you recieve it
-          console.log(someHTML)
+          const someHTML = template({stockData}) 
           $("#display-stocks").append(someHTML)
        }
 }
-
-const renderer = new Renderer()
- 
-const pageLoad = function(){
-  renderer.renderData(DudisObj)}
-
-pageLoad()
-
-
-// Handlebars.registerHelper('json', function(context) {
-//   return JSON.stringify(context);
-// });
