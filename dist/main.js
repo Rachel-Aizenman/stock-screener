04e8804274ff19db49fe4d4370aa567c00600aba
Ahmed <kahildude@gmail.com>
2019-11-13 19:03:22 +0200

    1

--- dist/main.js
+++ dist/main.js
@@ -4,13 +4,13 @@ const stocksManager = new StockManager();
 const renderer = new Renderer
 
 const handleSearch = async function(){
-    let input = $("#search-input").val()
-    $("#search-input").val("") 
+    let input = $("#stockSearch").val()
+    $("#stockSearch").val("") 
     await stocksManager.getStockData(input)
     renderer.renderData(stocksManager.stockData)
 }
 
-$("#search").on("click",async function () {
+$("#button").on("click",async function () {
     await handleSearch()
 })
 const rc= new RatioCalculator();
