74defb5e93e26c43815be76f51f3baba100c2afb
Ahmed <kahildude@gmail.com>
2019-11-13 12:11:53 +0200

    H

--- dist/stockManager.js
+++ dist/stockManager.js
@@ -7,15 +7,17 @@ class StockManager {
     }
     async getStockData(input) {
         let stock = await $.get('/stock/'+ input)
+        stock["ratios"]=ratioCalculator.calculateRatios(stock)
+        stock["ranks"]=analyzer.analyzeStock(stock)
         this.stockData.push(stock)
     }
     async getDataFromDB() {
         let stocksData = await $.get('/stocks')
         stocksData.forEach(s => {
-            this.stockData.push(s)
             s["ratios"]=ratioCalculator.calculateRatios(s)
             s["ranks"]=analyzer.analyzeStock(s)
+            this.stockData.push(s)
             console.log(s)
         })
     }
-    }    
+}    
